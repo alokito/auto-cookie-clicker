d84eca04f9b46bb94c5f01e746a5639c060a821b
Alokito <aloksaldanha@gmail.com>
2020-06-07 05:39:13 -0400

    Add buffs, decrease planting interval

--- gardening.js
+++ gardening.js
@@ -1,14 +1,28 @@
 
 function scoreBuff(buff) {
   if ([
+    "Dragon Harvest",
+    "Extra cycles",
+    "Oiled-up",
+    "Congregation",
+    "Refactoring",
+    "Breakthrough",
     "Macrocosm",
     "Manabloom",
     "Fervent adoration",
     "Frenzy",
     "Righteous cataclysm",
     "Luxuriant harvest",
+    "Solar flare",
+    "Delicious lifeforms",
+    "Winning streak",
+    "High-five",
     "Golden ages"].indexOf(buff) >= 0) {
     return 1;
+  } else if (buff == 'Elder frenzy') {
+    return 2;
+  } else if (buff == 'Cursed finger') {
+    return -10;
   } else if (['Click frenzy', 'Cookie storm'].indexOf(buff) >= 0) {
     return 0;
   } else if (buff == 'Clot') {
@@ -30,7 +44,7 @@ function totalBuffMult() {
 }
 
 window.keepHarvesting = 1;
-window.keepHarvestingPlant = mg.plantsById[8];
+window.keepHarvestingPlant = Game.ObjectsById[2].minigame.plantsById[8];
 function checkHarvest() {
   let wait = 1000;
   const mg = Game.ObjectsById[2].minigame;
@@ -62,14 +76,14 @@ function checkHarvest() {
           const bb = window.keepHarvestingPlant;
           console.log(`Planting ${bb.name} in col ${i} row ${j}, ${context} at ${new Date()}`);
           mg.useTool(bb.id, i, j);
-          window.keepHarvesting && setTimeout(checkHarvest, 1000);
+          window.keepHarvesting && setTimeout(checkHarvest, 100);
           return;
         }
       }
     }
-    // console.log(`nothing to plant? ${context}`);
+     // console.log(`nothing to plant? ${context}`);
   } else {
-    // console.log(`not enough buffs, ${context}`);
+     // console.log(`not enough buffs, ${context}`);
   }
   window.keepHarvesting && setTimeout(checkHarvest, 1000);
 }
