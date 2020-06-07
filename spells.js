bc273ff4f6289dcad6238627aee2985e74ef744a
silverninjasnowman <66573025+silverninjasnowman@users.noreply.github.com>
2020-06-07 19:02:43 -0400

    Update spells.js

--- spells.js
+++ spells.js
@@ -4,6 +4,7 @@ function checkSpells() {
   const spell = mg.spellsById[3];
   // change the id in the line right above this to cast different spells.
   if (mg.magic >= mg.magicM) {
+    console.log(`Casting ${spell.name} at ${new Date()}`);
     mg.castSpell(spell);
   }
   window.keepCasting && setTimeout(checkSpells, 1000);
