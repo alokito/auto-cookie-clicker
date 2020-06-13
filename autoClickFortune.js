cb5616ef7aaba1a52b4229ef44598940a961a65a
Alokito <aloksaldanha@gmail.com>
2020-06-13 14:37:08 -0400

    Improved log message for fortune cookies

--- autoClickFortune.js
+++ autoClickFortune.js
@@ -2,7 +2,7 @@ window.keepCheckingfortune = 1
 function checkFortune() {
   if (Game.TickerEffect && Game.TickerEffect.type=='fortune'){
     Game.tickerL.click();
-    console.log(`clicked fortune ${Game.tickerL.innerHTML} at ${new Date()}`)
+    console.log(`clicked fortune ${Game.tickerL.textContent} granting ${Game.TickerEffect.sub} at ${new Date()}`)
   }
   setTimeout(checkFortune, 7000);
 }
