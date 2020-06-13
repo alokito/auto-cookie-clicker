8eed0b975a9a7710757b31937ed33b78dbf02058
silverninjasnowman <66573025+silverninjasnowman@users.noreply.github.com>
2020-06-13 14:22:12 -0400

    Create autoclick fortune.js

---
+++ autoclick fortune.js
@@ -0,0 +1,8 @@
+window.keepCheckingfortune = 1
+function checkFortune() {
+  if (Game.TickerEffect && Game.TickerEffect.type=='fortune'){
+    Game.tickerL.click();
+    console.log(`clicked fortune ${Game.tickerL.innerHTML} at ${new Date()}`)
+  }
+  setTimeout(checkFortune, 7000);
+}
